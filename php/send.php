d828ee747551065913af3a7307a9d7110d9ade8b
Jose D'lucas <jsdlcs266@gmail.com>
2021-07-03 03:21:00 -0400

    a

--- php/send.php
+++ php/send.php
@@ -1,27 +1,29 @@
 <?php
 session_start();
-require $_SERVER['DOCUMENT_ROOT'] . '/../vendor/autoload.php';
+require $_SERVER['DOCUMENT_ROOT'] . '../vendor/autoload.php';
 
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\SMTP;
 use PHPMailer\PHPMailer\Exception;
 use Rakit\Validation\Validator;
 
-
+request($_POST['email'],$_POST['full_name'],'Contacto Buenbit Pro');
 function request($mail_from_address, $mail_from_name, $mail_subject){
     $validator = new Validator;
 
     try {
         $full_name = $_POST['full_name'];
         $email = $_POST['email'];
-        $business_name = $_POST['business_name'];
-        $message = htmlspecialchars($_POST['message']);
+        $interes = $_POST['interes'];
+        $cuotas = $_POST['cuotas'];
+        $interes_total = $_POST['interes_total'];
 
         $validation = $validator->validate($_POST, [
             'full_name' => 'required|max:128',
             'email'     => 'required|email',
-            'business_name' =>'required|max:128',
-            'message'   => 'required|max:1500'
+            'interes' =>'required',
+            'cuotas'   => 'required',
+            'interes_total' => 'required'
         ]);
 
         $validation->validate();
@@ -38,53 +40,53 @@ function request($mail_from_address, $mail_from_name, $mail_subject){
             header('Location: /index.php');
             exit(0);
         }
-        sendForm($mail_from_address, $mail_from_name, $mail_subject, $full_name, $email, $business_name, $message);
+
+        sendForm($mail_from_address, $mail_from_name, $mail_subject, $full_name, $email, $interes, $cuotas, $interes_total);
     } catch (Exception $e) {
         header('HTTP/1.1 500 Internal Server Error');
         exit(0);
     }
 }
 
-function sendForm($mail_from_address, $mail_from_name, $mail_subject, $full_name, $email, $business_name, $message ){
+function sendForm($mail_from_address, $mail_from_name, $mail_subject, $full_name, $email, $interes, $cuotas, $interes_total){
     try {
         $mail = new PHPMailer();
         $mail->SMTPDebug = SMTP::DEBUG_OFF;
         $mail->isSMTP();
         //datos de acceso al sevidor smtp <<<INIT>>>
-        $mail->Host       = getenv('SMTP_HOST');
+        $mail->Host       = 'smtp.gmail.com';
         $mail->SMTPAuth   = true;
-        $mail->Username   = getenv('SMTP_USER');
-        $mail->Password   = getenv('SMTP_PASSWORD');
+        $mail->Username   = 'XXXXXXXXX';
+        $mail->Password   = 'XXXXXXXXXX';
         $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
         $mail->Port       = 587;
         $mail->IsHTML(true);
         //<<<END>>>
 
         // Emisor de email
-        //$mail->setFrom('contacto@pro.buenbit.com', 'Contacto Buenbit Pro');
-        $mail->setFrom($mail_from_address, $mail_from_name);
+        $mail->setFrom('jsdlcs266@gmail.com', 'Contacto Buenbit Pro');
+        //$mail->setFrom($mail_from_address, $mail_from_name);
         // Establecer una dirección de respuesta alternativa
         $mail->addReplyTo($email, $full_name);
         // Establecer a quién se enviará el mensaje
-        $mail->addAddress('pro@buenbit.com', 'Buenbit Pro');
+        //$mail->addAddress('pro@buenbit.com', 'Buenbit Pro');
+        $mail->addAddress('jsdlcs266@gmail.com', 'Buenbit Pro');
         // Asunto
         $mail->Subject = $mail_subject; // '[BBPro.pe] Contacto';
         // Mensaje
-        $body = file_get_contents($_SERVER['DOCUMENT_ROOT'] . '/../templates/mail_template.html');
+        $body = file_get_contents($_SERVER['DOCUMENT_ROOT'] . '../email_plantilla.html');
         $body = str_replace('%full_name%', $full_name, $body);
         $body = str_replace('%email%', $email, $body);
-        $body = str_replace('%business_name%', $business_name, $body);
-        $body = str_replace('%message%', $message, $body);
-        $body = str_replace('%date%', date('d/m/Y', time()), $body);
+        $body = str_replace('%interes%', $interes, $body);
+        $body = str_replace('%cuotas%', $cuotas, $body);
+        $body = str_replace('%interes_total%', $interes_total, $body);
 
         //$mail->Body =  $msg;
         $mail->MsgHTML($body);
         $mail->send();
-        header("Location:" . $_SERVER['HTTP_REFERER']);
+        header("Location:../gracias.php");
     } catch (Exception $e) {
         header('HTTP/1.1 500 Internal Server Error');
         exit(0);
     }
 }
-
-?>
